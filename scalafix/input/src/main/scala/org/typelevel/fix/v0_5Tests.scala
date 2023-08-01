810cd65cac5fd1e3b46560cea1dd7ce1a27cc58c
David Gregory <2992938+DavidGregory084@users.noreply.github.com>
2023-08-01 14:42:22 +0100

    Improve the 0.5.x migration by handling some more wildcard import possibilities

--- scalafix/input/src/main/scala/org/typelevel/fix/v0_5Tests.scala
+++ scalafix/input/src/main/scala/org/typelevel/fix/v0_5Tests.scala
@@ -5,9 +5,15 @@ rule = v0_5
 import io.github.davidgregory084.TpolecatPlugin
 import io.github.davidgregory084.{ScalaVersion => Version}
 import io.github.davidgregory084.{DevMode => Dev}
+import io.github.davidgregory084.TpolecatPlugin._
+import io.github.davidgregory084.TpolecatPlugin.autoImport
 import io.github.davidgregory084.TpolecatPlugin.autoImport._
 import io.github.davidgregory084.TpolecatPlugin.autoImport.{ tpolecatCiModeEnvVar, scalacOptionsFor }
 import io.github.davidgregory084._
+import io.github.davidgregory084.ScalaVersion._
+import io.github.davidgregory084.ScalaVersion.V2_11_0
+import io.github.davidgregory084.TpolecatPlugin.autoImport.ScalacOptions._
+import io.github.davidgregory084.TpolecatPlugin.autoImport.ScalacOptions.checkInit
 import _root_.io.github.davidgregory084.ScalacOption
 import _root_.io.github.davidgregory084._
 // format: on
